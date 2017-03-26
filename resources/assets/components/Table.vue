3d8632976b21707079b36ccc650b423ca2659535
klgd <slice1213@gmail.com>
2017-03-26 22:29:43 +0800

    develop admin views

--- resources/assets/components/Table.vue
+++ resources/assets/components/Table.vue
@@ -105,7 +105,10 @@
             }
         },
         props: {
-
+            columns: {
+                type: Object,
+                default () => {}
+            }
         }
     }
 </script>
