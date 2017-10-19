39d9ffd0fd2bf59e056055140a29310d5ffbc73b
klgd <slice1213@gmail.com>
2017-10-19 18:30:35 +0800

    view image type

--- resources/assets/views/model/box/tree.vue
+++ resources/assets/views/model/box/tree.vue
@@ -4,14 +4,36 @@
         <!-- /.box-header -->
         <!--<v-table></v-table>-->
         <div class="box-body table-responsive">
-
+            <div class="dd">
+                <ol class="dd-list">
+                    <li class="dd-item" data-id="1">
+                        <div class="dd-handle">Item 1</div>
+                    </li>
+                    <li class="dd-item" data-id="2">
+                        <div class="dd-handle">Item 2</div>
+                    </li>
+                    <li class="dd-item" data-id="3">
+                        <div class="dd-handle">Item 3</div>
+                        <ol class="dd-list">
+                            <li class="dd-item" data-id="4">
+                                <div class="dd-handle">Item 4</div>
+                            </li>
+                            <li class="dd-item" data-id="5" data-foo="bar">
+                                <div class="dd-nodrag">Item 5</div>
+                            </li>
+                        </ol>
+                    </li>
+                </ol>
+            </div>
         </div>
-
     </div>
 </template>
 
 <script>
     import vHeader from './header.vue'
+    import 'nestable2/dist/jquery.nestable.min.css'
+
+    console.log($.fn)
 
     export default {
         name: 'vTree',
@@ -21,5 +43,10 @@
         components: {
             vHeader,
         },
+        mounted() {
+            console.log(window.jQuery.fn);
+            console.log($.fn.nestable);
+            $('.dd').nestable({ /* config options */ });
+        }
     }
 </script>
--- resources/assets/views/model/index.vue
+++ resources/assets/views/model/index.vue
@@ -13,6 +13,7 @@
         <div class="col-xs-12">
             <v-table v-if="config.view.type == 'table'"></v-table>
             <v-tree v-else-if="config.view.type == 'tree'"></v-tree>
+            <v-image v-else-if="config.view.type == 'image'"></v-image>
 
         </div>
     </div>
@@ -22,12 +23,14 @@
 
     import vTable from './box/table.vue'
     import vTree from './box/tree.vue'
+    import vImage from './box/image.vue'
     import mixins from './mixins'
 
     export default {
         components: {
             vTable,
             vTree,
+            vImage,
         },
         data() {
             return {
