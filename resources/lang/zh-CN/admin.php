5cbe456d15afe4ecd64d8d0e9649799d376ffbb7
klgd <slice1213@gmail.com>
2018-01-16 13:39:31 +0800

    lang file

--- resources/lang/zh-CN/admin.php
+++ resources/lang/zh-CN/admin.php
@@ -1,32 +1,4 @@
 <?php
 
 return [
-    'common' => [
-        'dashboard' => '控制台',
-    ],
-    'system' => [
-        'site_name_required' => '请输入网站名称',
-
-        'menu' => [
-            'name_required'         => '请输入名称',
-            'display_name_required' => '请输入显示名称',
-        ],
-    ],
-    'users'  => [
-        'username_required'         => '请输入用户名',
-        'username_rangelength'      => '用户名长度必须在{0}和{1}之间',
-        'password_required'         => '请输入密码',
-        'password_rangelength'      => '密码长度必须在{0}和{1}之间',
-        'repassword_required'       => '请输入密码',
-        'repassowrd_equal_password' => '两次密码输入不一致',
-        'role_required'             => '请选择角色',
-        'email_required'            => '请输入邮箱',
-        'email_format'              => '邮箱格式有误',
-
-        'role_name_required'         => '请输入角色名',
-        'role_display_name_required' => '请输入角色显示名称',
-
-    ],
-
-
 ];
